56ab7811cc009f84dcec36b191ba1f8ae34996f9
bastndev <gohit24@gmail.com>
2022-11-19 21:20:18 -0500

    Stelar

---
+++ js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener("mousemove",function(e){
+    var rocket = document.querySelector('.rocket');
+    rocket.style.left = e.offsetX +'px';
+    rocket.style.top = e.offsetY +'px';
+})
+
+function stars(){
+    let count = 50;
+    let scene = document.querySelector('.scene');
+    let i = 0;
+    while ( i < count){
+        let star = document.createElement('i');
+        let x = Math.floor(Math.random()*window.innerWidth);
+
+        let duration = Math.random()* 1;
+        let  h = Math.random() * 100;
+
+        star.style.left = x + 'px';
+        star.style.width = 1 + 'px';
+        star.style.height = 50 + h + 'px';
+        star.style.animationDuration = duration + 's';
+
+        scene.appendChild(star);
+        i++
+    }
+}
+stars();
